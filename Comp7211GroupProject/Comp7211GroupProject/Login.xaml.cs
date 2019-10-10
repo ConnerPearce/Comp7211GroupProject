0accbe57e8cfdfc80ceb9a079479676998d98d3b
Jot Sidhu <27036778@student.toiohomai.ac.nz>
2019-10-10 16:45:35 +1300

    Added Login Page & Graduation Cap Image

---
+++ Comp7211GroupProject/Comp7211GroupProject/Login.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Comp7211GroupProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+    }
+}
