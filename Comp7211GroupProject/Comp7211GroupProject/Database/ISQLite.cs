5de2318941004bbbe2360e0bfbc6a7be04491c56
Kawhina Hohepa <27037080@student.toiohomai.ac.nz>
2019-10-10 14:00:07 +1300

    Created database and established connection, and created 'UserModel' class and two folders 'models' and 'database'

---
+++ Comp7211GroupProject/Comp7211GroupProject/Database/ISQLite.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Comp7211GroupProject.Database
+{
+    public interface ISQLite
+    {
+        SQLiteConnection GetConnection();
+    }
+}
---
+++ Comp7211GroupProject/Comp7211GroupProject/Database/SQLite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SQLite;
+using SQLitePCL;
+
+namespace Comp7211GroupProject.Database
+{
+    public class SQLite : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var db = "AppDatabase";
+            var path = Path.Combine(System.Environment.GetFolderPath
+                (System.Environment.SpecialFolder.Personal), db);
+
+            return new SQLiteConnection(path);
+        }
+    }
+}
