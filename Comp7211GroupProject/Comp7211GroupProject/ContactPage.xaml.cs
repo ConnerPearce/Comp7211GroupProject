61cd5c8c8e884434cff818d973c05f5275a2f481
Jot Sidhu <27036778@student.toiohomai.ac.nz>
2019-10-13 15:45:44 +1300

    Made Navigation Bar for Contact Page. Finished Form UI, & changed background of Contact Page Navigation bar to White.

--- Comp7211GroupProject/Comp7211GroupProject/ContactPage.xaml.cs
+++ Comp7211GroupProject/Comp7211GroupProject/ContactPage.xaml.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+using Xamarin.Forms.PlatformConfiguration;
 
 namespace Comp7211GroupProject
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ContactPage : ContentPage
+    public partial class ContactPage : Xamarin.Forms.TabbedPage
     {
         public ContactPage()
         {
+            On<Android>().SetBarSelectedItemColor(Color.Black);
+            On<Android>().SetBarItemColor(Color.FromHex("#737373"));
             InitializeComponent();
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
