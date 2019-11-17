3f158b5cc2444cacf0e13b21c2062b9d31cb2545
Karl <karl.perez94@gmail.com>
2019-11-18 11:38:36 +1300

    Added a list to the HomeBackend as well as a Get List method

--- Comp7211GroupProject/Comp7211GroupProject/Classes/HomePage/HomeBackend.cs
+++ Comp7211GroupProject/Comp7211GroupProject/Classes/HomePage/HomeBackend.cs
@@ -1,6 +1,8 @@
-﻿using Comp7211GroupProject.Classes.API.Proxys;
+﻿using Comp7211GroupProject.Classes.API.Models;
+using Comp7211GroupProject.Classes.API.Proxys;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Comp7211GroupProject.Classes.HomePage
@@ -10,9 +12,29 @@ namespace Comp7211GroupProject.Classes.HomePage
         // Use for accessing API
         IPostProxy _postProxy;
 
+
         public HomeBackend(IPostProxy postProxy)
         {
             _postProxy = postProxy;
         }
+
+        private ObservableCollection<IPosts> PostList = new ObservableCollection<IPosts>(){ };//The List for where we will store the data from api
+
+        public async void PostInfo()//this code is where the data from the api will be added to thew list
+        {
+            var temp = await _postProxy.GetAllPosts();
+            foreach (var item in temp)
+            {
+                PostList.Add(item);
+            }
+        }
+
+        public ObservableCollection<IPosts> GetPostList
+        {
+            //the front end will this method to display whats in the list
+            get { return PostList; }
+        }
+
+
     }
 }
