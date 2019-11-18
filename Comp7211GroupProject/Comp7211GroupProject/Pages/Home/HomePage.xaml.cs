ff682409c3dd7209782e4ea6745a10ef12851b90
Conner Pearce <metalichking@outlook.com>
2019-11-19 11:33:21 +1300

    Can now post posts

--- Comp7211GroupProject/Comp7211GroupProject/Pages/Home/HomePage.xaml.cs
+++ Comp7211GroupProject/Comp7211GroupProject/Pages/Home/HomePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 using Comp7211GroupProject.Classes.API.Models;
+using Comp7211GroupProject.Classes.API.Proxys;
 using Comp7211GroupProject.Classes.HomePage;
 using System;
 using System.Collections.Generic;
@@ -16,8 +17,8 @@ namespace Comp7211GroupProject
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        private IContainer container;
-        List<Posts> posts = new List<Posts>();
+
+        PostProxy postsProxy = new PostProxy("https://comp7211groupprojectapi20191115092109.azurewebsites.net/");
 
         public HomePage()
         {
@@ -25,33 +26,6 @@ namespace Comp7211GroupProject
             stackCreatePost.IsVisible = false;
             stackPosts.IsVisible = true;
 
-          // GetPosts();
-        }
-
-        private async void GetPosts()
-        {
-            try
-            {
-                container = DependancyInjection.Configure();
-
-                using (var scope = container.BeginLifetimeScope())
-                {
-                    var app = scope.Resolve<IHomeBackend>();
-
-                    var temp = app.PostList;
-                    if (temp != null)
-                    {
-                        posts = temp;
-                    }
-                    else
-                        await DisplayAlert("Error", "There was an error retrieving the posts, Reload the page", "Ok");
-
-                }
-            }
-            catch
-            {
-                await DisplayAlert("Error", "There was an error retrieving the posts, Reload the page", "Ok");
-            }
         }
 
         private void btnCreatePost_Clicked(object sender, EventArgs e)
@@ -74,12 +48,14 @@ namespace Comp7211GroupProject
             //Send Created Post to Listview
             stackCreatePost.IsVisible = false;
             stackPosts.IsVisible = true;
-            if (String.IsNullOrEmpty(txtMessage.Text))
+            if (!String.IsNullOrEmpty(txtMessage.Text))
             {
-                await DisplayAlert("Error", "The Message box must not be left empty!, Try again", "Ok");
+                string response = await postsProxy.PostPosts(new Posts { Post = txtMessage.Text, Uid = 1 });
+                await DisplayAlert("Progress", response , "Ok");
             }
             else
-                await Navigation.PopModalAsync();
+                await DisplayAlert("Error", "The Message box must not be left empty!, Try again", "Ok");
+
         }
     }
 }
